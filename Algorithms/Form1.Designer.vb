4e52b2bd5fcd20da85219d2d1f3c33c81739569d
Anastasiya <girl-physicist@live.com>
2019-03-24 13:31:50 +0300

    Euclidean and Zeno algorithm

--- Algorithms/Form1.Designer.vb
+++ Algorithms/Form1.Designer.vb
@@ -22,10 +22,53 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ButtonSimpleAlgorithms = New System.Windows.Forms.Button()
+        Me.ButtonZenoAlgorithm = New System.Windows.Forms.Button()
+        Me.ButtonEuclideanAlgorithm = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonSimpleAlgorithms
+        '
+        Me.ButtonSimpleAlgorithms.Location = New System.Drawing.Point(12, 72)
+        Me.ButtonSimpleAlgorithms.Name = "ButtonSimpleAlgorithms"
+        Me.ButtonSimpleAlgorithms.Size = New System.Drawing.Size(215, 23)
+        Me.ButtonSimpleAlgorithms.TabIndex = 5
+        Me.ButtonSimpleAlgorithms.Text = "SimpleAlgorithms"
+        Me.ButtonSimpleAlgorithms.UseVisualStyleBackColor = True
+        '
+        'ButtonZenoAlgorithm
+        '
+        Me.ButtonZenoAlgorithm.Location = New System.Drawing.Point(12, 42)
+        Me.ButtonZenoAlgorithm.Name = "ButtonZenoAlgorithm"
+        Me.ButtonZenoAlgorithm.Size = New System.Drawing.Size(215, 23)
+        Me.ButtonZenoAlgorithm.TabIndex = 4
+        Me.ButtonZenoAlgorithm.Text = "ZenoAlgorithm"
+        Me.ButtonZenoAlgorithm.UseVisualStyleBackColor = True
+        '
+        'ButtonEuclideanAlgorithm
+        '
+        Me.ButtonEuclideanAlgorithm.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonEuclideanAlgorithm.Name = "ButtonEuclideanAlgorithm"
+        Me.ButtonEuclideanAlgorithm.Size = New System.Drawing.Size(215, 23)
+        Me.ButtonEuclideanAlgorithm.TabIndex = 3
+        Me.ButtonEuclideanAlgorithm.Text = "EuclideanAlgorithm"
+        Me.ButtonEuclideanAlgorithm.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(238, 109)
+        Me.Controls.Add(Me.ButtonSimpleAlgorithms)
+        Me.Controls.Add(Me.ButtonZenoAlgorithm)
+        Me.Controls.Add(Me.ButtonEuclideanAlgorithm)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ButtonSimpleAlgorithms As Button
+    Friend WithEvents ButtonZenoAlgorithm As Button
+    Friend WithEvents ButtonEuclideanAlgorithm As Button
 End Class
---
+++ Algorithms/SimpleAlgorithmsForm.vb
@@ -0,0 +1,62 @@
+﻿Public Class SimpleAlgorithmsForm
+
+    Dim Generator As Random = New Random()
+    Dim Arr(6, 6) As Integer
+    Private Sub ButtonInit_Click(sender As Object, e As EventArgs) Handles ButtonInit.Click
+
+        For i = 0 To 5
+            DataGridView1.Columns.Add("", "")
+            DataGridView1.Rows.Insert(i)
+        Next
+        For i = 0 To 5
+            For j = 0 To 5
+                Arr(i, j) = Generator.Next(500)
+                DataGridView1.Item(i, j).Value = Arr(i, j)
+            Next
+        Next
+
+    End Sub
+
+    Private Sub ButtonSortDescending_Click(sender As Object, e As EventArgs) Handles ButtonSortDescending.Click
+        'Bubble sort 
+        'Shaker sort
+        'Comb sort
+        'Insertion sort
+        'Shellsort
+        'Tree sort
+        'Gnome sort
+        'Selection sort
+        'and other types of sorting (https://habr.com/ru/post/335920/)
+    End Sub
+
+    Private Sub ButtonSortAscending_Click(sender As Object, e As EventArgs) Handles ButtonSortAscending.Click
+
+    End Sub
+
+    Private Sub ButtonMin_Click(sender As Object, e As EventArgs) Handles ButtonMin.Click
+        Dim Min As Single
+        Min = Arr(0, 0)
+        For i = 0 To 5
+            For j = 0 To 5
+                If Min > Arr(i, j) Then
+                    Min = Arr(i, j)
+                End If
+            Next
+        Next
+        ButtonMin.Text = "Min = " + Min.ToString
+    End Sub
+
+    Private Sub ButtonMax_Click(sender As Object, e As EventArgs) Handles ButtonMax.Click
+        Dim Max As Single
+        Max = Arr(0, 0)
+        For i = 0 To 5
+            For j = 0 To 5
+                If Max < Arr(i, j) Then
+                    Max = Arr(i, j)
+                End If
+            Next
+        Next
+        ButtonMax.Text = "Max = " + Max.ToString
+    End Sub
+End Class
+
